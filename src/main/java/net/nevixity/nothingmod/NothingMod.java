d9ac48df49da7abe82ae0c8543a0e2beef2ba4f5
nevixity1 <tomerbaryo09@gmail.com>
2023-11-04 22:58:12 +0200

    KEEEEEEEP

--- src/main/java/net/nevixity/nothingmod/NothingMod.java
+++ src/main/java/net/nevixity/nothingmod/NothingMod.java
@@ -16,7 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NothingMod implements ModInitializer {
-public static final String MOD_ID = "nothingmod";
+	public static final String MOD_ID = "nothingmodsmp";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
