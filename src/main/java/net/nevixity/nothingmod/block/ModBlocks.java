ab01e0b1b19d85f3681f21ab589252183ddc394f
nevixity1 <tomerbaryo09@gmail.com>
2023-11-19 10:59:36 +0200

    KEEPING

--- src/main/java/net/nevixity/nothingmod/block/ModBlocks.java
+++ src/main/java/net/nevixity/nothingmod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -11,13 +12,14 @@ import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.nevixity.nothingmod.NothingMod;
 
 public class ModBlocks {
     public static final Block ODIUM_BLOCK = registerBlock("odium_block", new Block(FabricBlockSettings.copyOf(Blocks.NETHERITE_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)),
             new FabricItemSettings().fireproof().rarity(Rarity.EPIC));
 
-    public static final Block ODIUM_ORE = registerBlock("odium_ore", new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_BLOCK).sounds(BlockSoundGroup.MEDIUM_AMETHYST_BUD)),
+    public static final Block ODIUM_ORE = registerBlock("odium_ore", new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DIAMOND_BLOCK).sounds(BlockSoundGroup.MEDIUM_AMETHYST_BUD), UniformIntProvider.create(4, 7)),
             new FabricItemSettings().fireproof().rarity(Rarity.EPIC));
 
     private static Block registerBlock(String id, Block block) {
